36aee02ce6bf2a5409c3186161a6774ad588c208
Ryan Do <falcsmike7@gmail.com>
2017-11-04 18:21:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ const returnLastTwoDrivers = function (array) {
 }
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (integer) {
+  return function (fare) {
+    return fare * integer;
+  }
+}
